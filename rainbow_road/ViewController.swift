2f9d827311041d4fee8cfbe30adcc1e71c3f6d15
ruslanvs <ruslanvs@yahoo.com>
2018-03-12 17:26:44 -0700

    Initial commit

---
+++ rainbow_road/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  rainbow_road
+//
+//  Created by Ruslan Suvorov on 3/12/18.
+//  Copyright © 2018 Ruslan Suvorov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
